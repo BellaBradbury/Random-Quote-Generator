f8af5d17793d2d3d0c93b25058ac375c2de19c59
BellaBradbury <bellabradbury@yahoo.com>
2022-05-09 10:58:42 -0400

    SCRIPT.JS step 4
    quote array added

--- js/script.js
+++ js/script.js
@@ -3,14 +3,34 @@ Treehouse Techdegree:
 Project 4 - Random Quote Generator
 *************************************************/
 
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community
-
-/*** 
- * `quotes` array 
+/***
+ * `quotes` array
 ***/
-
+const quotes = [
+  {
+    quote: "Principles aren't principles if you pick and choose when to use them.",
+    source: 'Chidi Anagonye',
+    citation: 'The Good Place'
+  },
+  {
+    quote: "Oh, I hope you're happy, but not like how you were with me. I'm selfish, I know. I can't let you go. So find someone great, but don't find no one better. I hope you're happy, but don't be happier.",
+    source: 'Olivia Rodrigo',
+    year: '2021'
+  },
+  {
+    quote: "It is better to change an opinion than to persist in a wrong one.",
+    source: 'Socrates',
+    location: 'Athens, Greece'
+  },
+  {
+    quote: "All I've Ever Known is how to hold my own",
+    source: 'Eurydice'
+  },
+  {
+    quote: "Just give it time. It's gonna get better. Now is not forever at all.",
+    source: 'Jon McLaughlin'
+  }
+];
 
 
 /***
